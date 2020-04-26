da980ed02ee048ce31ed3d1969b88ae8cba3c8dd
Michael Dalton <modintexas@gmail.com>
2020-04-25 23:38:28 -0700

    Add dotenv support for differnt build enviornments

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   devtool: 'source-map',
@@ -42,6 +43,7 @@ module.exports = {
       filename: 'index.html',
       template: './src/index.html'
     }),
+    new Dotenv()
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
