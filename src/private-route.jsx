13a48c59b1bacabf9dfd4aeeb8d670e70b77f79a
Michael Dalton <modintexas@gmail.com>
2020-04-25 23:42:43 -0700

    Private Route support in ReactRouter, Authenticated requests

---
+++ src/private-route.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+import { isAuthenticated } from './auth-req';
+
+
+const PRoute = ({ children, history, ...rest }) => {
+  console.log(rest)
+  return(
+  <Route
+    {...rest}
+    render={({ location }) =>
+            isAuthenticated(history) ? (
+              children
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/login",
+                  state: { from: location }
+                }}
+              />
+            )
+           }
+  />
+)};
+
+export default withRouter(PRoute);
