933695f6d5353126927c60bd867e830936f63067
Michael Dalton <modintexas@gmail.com>
2020-05-02 19:50:17 -0700

    Small navbar changes. still needs css work

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,59 @@
-import React from "react";
-import { Drawer, List, ListItem } from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
-import MapIcon from '@material-ui/icons/Map';
+import React, { useState } from "react";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from "@material-ui/core";
+import MapIcon from "@material-ui/icons/Map";
+
 import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link, useLocation } from "react-router-dom";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 const useStyles = makeStyles(theme => ({
   paper: {
     backgroundColor: "#c1c1c1"
+  },
+  icon: {
+    height: "35px"
   }
 }));
 
-export default () => {
+const NavItem = ({ iconOnly, path, children, label }) => {
   const classes = useStyles();
   const { pathname } = useLocation();
+  return (
+    <ListItem component={Link} to={path} selected={pathname == path}>
+      <ListItemIcon className={classes.icon}>{children}</ListItemIcon>
+      <ListItemText hidden={iconOnly} primary={label} />
+    </ListItem>
+  );
+};
+
+export default () => {
+  const classes = useStyles();
+  const [hover, setHover] = useState(false);
+
   return (
     <Drawer
       open
       anchor="left"
       variant="permanent"
       classes={{ paper: classes.paper }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
       <List>
-        <ListItem selected={pathname == "/map"}>
-          <IconButton component={Link} to="/map">
-            <MapIcon />
-          </IconButton>
-        </ListItem>
-        <ListItem>
-          <IconButton selected={pathname == "/profile"} component={Link} to="/profile">
-            <PersonOutlineOutlinedIcon />
-          </IconButton>
-        </ListItem>
+        <NavItem iconOnly={!hover} path={"/map"} label="Map">
+          <MapIcon />
+        </NavItem>
+        <NavItem iconOnly={!hover} path={"/profile"} label="Profile">
+          <PersonOutlineOutlinedIcon />
+        </NavItem>
       </List>
     </Drawer>
   );
